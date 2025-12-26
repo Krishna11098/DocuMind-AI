6ab79ae67a5477a80f9735690eae4ea642c2bccc
Krishna Tahiliani <krishna.tahiliani2006@gmail.com>
2025-12-27 03:21:14 +0530

    basic changes

--- backend/test.py
+++ backend/test.py
@@ -1,8 +1,1 @@
-import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCJb4wGb3Swah6aVfQ4pK8_cnnb423zf1U")
-
-
-# List available models
-for model in genai.list_models():
-    print(model.name)
